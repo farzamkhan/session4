3ff25cfd75467fb56c2f166ad33bf48b4bdf61f0
farzamkhan <farzam@live.com>
2019-07-21 09:18:45 +0430

    loop

---
+++ tamin1.py
@@ -0,0 +1,9 @@
+import hashlib
+
+my_input = None
+while my_input != "exit" :
+    my_input = input ("Enter your text:")
+    if my_input != "exit":
+        my_encode = hashlib.md5(my_input.encode())
+        print(my_encode)
+
